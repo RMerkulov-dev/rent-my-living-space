f1c756a55119666fd59549f2c2cc243688a4ca3b
RMerkulov <fotoromario@gmail.com>
2023-05-03 16:58:31 +0300

    start search

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import LoginModal from "@/app/components/modals/LoginModal";
 import ToasterProvider from "@/app/providers/ToasterProvider";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import RentModal from "@/app/components/modals/RentModal";
+import SearchModal from "@/app/components/modals/SearchModal";
 
 export const metadata = {
   title: "RMLS",
@@ -28,6 +29,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <SearchModal />
         <RentModal />
         <LoginModal />
         <RegisterModal />
