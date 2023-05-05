4efd9a44c8aa3f67b4e2dec2f8b5e9d0ff91ba36
RMerkulov <fotoromario@gmail.com>
2023-05-05 16:10:36 +0300

    upd logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = () => {
       <Image
         onClick={() => router.push("/")}
         alt="Logo"
-        className="hidden md:block cursor-pointer"
+        className=" md:block cursor-pointer"
         height="50"
         width="50"
         src="/images/main_logo.png"
